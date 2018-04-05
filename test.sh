e56d90b5688f4b16a89147fa5781c24ac83d2a92
Tom Szilagyi <tomszilagyi@gmail.com>
2018-04-05 22:07:48 +0200

    Fix test script error reporting on travis-ci
    The travis-ci build was not marked as failed even though the test
    script detected an error. The problem was the (otherwise normal) usage
    of the $? bash variable, that conflicts with the way travis-ci runs
    the script.
    
    See: https://github.com/travis-ci/travis-ci/issues/3771

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-lein test
-if [ $? -ne 0 ]; then
-    exit $?
+lein test; export RC=$?
+if [ $RC -ne 0 ]; then
+    exit $RC
 fi
 
 echo
@@ -15,10 +15,10 @@ rm -f test/data/*.out
 echo -n "testing bb.csv... "
 lein run -l $LEDGER -f test/data/bb.csv -sa 3 -sz 2 -D 'yyyy/MM/dd' \
      -r 3 -m 4 -t '%9!%1 %6 %7 %8' -a 'Assets:BB Account' -c HUF > test/data/bb.out
-diff -u test/data/bb.out test/data/bb.ref-out >/dev/null
-if [ $? -ne 0 ]; then
+diff -u test/data/bb.out test/data/bb.ref-out >/dev/null; export RC=$?
+if [ $RC -ne 0 ]; then
     echo "FAIL, inspect bb.out!"
-    exit $?
+    exit $RC
 else
     rm test/data/bb.out
     echo "OK"
@@ -27,10 +27,10 @@ fi
 echo -n "testing ica.csv... "
 lein run -l $LEDGER -f test/data/ica.csv -F ';' -sa 1 -m 4 -t '%1' \
 	-a 'Assets:ICA Account' > test/data/ica.out
-diff -u test/data/ica.out test/data/ica.ref-out >/dev/null
-if [ $? -ne 0 ]; then
+diff -u test/data/ica.out test/data/ica.ref-out >/dev/null; export RC=$?
+if [ $RC -ne 0 ]; then
     echo "FAIL, inspect ica.out!"
-    exit $?
+    exit $RC
 else
     rm test/data/ica.out
     echo "OK"
@@ -39,10 +39,10 @@ fi
 echo -n "testing seb.csv... "
 lein run -l $LEDGER -f test/data/seb.csv -sa 5 -r 2 -m 4 -t '%3' \
      -a 'Assets:SEB Account' > test/data/seb.out
-diff -u test/data/seb.out test/data/seb.ref-out >/dev/null
-if [ $? -ne 0 ]; then
+diff -u test/data/seb.out test/data/seb.ref-out >/dev/null; export RC=$?
+if [ $RC -ne 0 ]; then
     echo "FAIL, inspect seb.out!"
-    exit $?
+    exit $RC
 else
     rm test/data/seb.out
     echo "OK"
